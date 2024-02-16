1638b654c2ff2354073a6190686d1429251508be
shpetimsedolli <shpeti101@gmail.com>
2024-02-16 17:24:55 +0100

    more arrow up to go top of a page

--- assets/js/main.js
+++ assets/js/main.js
@@ -3,6 +3,25 @@ var loader = document.getElementById("preloader");
 var timeout = setTimeout(function () {
   loader.style.display = "none";
 }, 2000);
+
 function scrollToBottom() {
   window.scroll(0, document.body.scrollHeight);
+}
+
+window.onscroll = function () {
+  scrollFunction();
+};
+
+function scrollFunction() {
+  var scrollToTopbtn = document.getElementById("scrollToTopbtn");
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    scrollToTopbtn.style.display = "block";
+  } else {
+    scrollToTopbtn.style.display = "none";
+  }
+}
+function scrollToTop() {
+  document.body.scrollTop = 0;
+
+  document.documentElement.scrollTop = 0;
 }
