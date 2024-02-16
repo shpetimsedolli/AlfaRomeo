d4bb0971e1b1be537fbbe942b093d634e41773c9
shpetimsedolli <shpeti101@gmail.com>
2024-02-16 17:08:15 +0100

    added button to go bottom of a page

--- assets/js/main.js
+++ assets/js/main.js
@@ -3,3 +3,6 @@ var loader = document.getElementById("preloader");
 var timeout = setTimeout(function () {
   loader.style.display = "none";
 }, 2000);
+function scrollToBottom() {
+  window.scroll(0, document.body.scrollHeight);
+}
